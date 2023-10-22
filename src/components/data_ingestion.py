ee7ecf827a81146bab85617ba423884441da1e05
vinayak1514 <vinayakjangir1514@gmail.com>
2023-10-22 08:56:13 +0530

    make data ingestion file

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,52 @@
+import pandas as pd 
+import sys
+import os 
+from sklearn.model_selection import train_test_split
+from src.exception import CustomException
+from src.logger import logging
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig():
+    """
+    this class make artifact folder make directory
+    """
+    train_data_path:str = os.path.join('artifacts','train.csv')
+    test_data_path:str = os.path.join('artifacts','test.csv')
+    raw_data_path:str = os.path.join('artifacts','data.csv')
+
+"""
+this class use for data ingestion , ingestion_config use artifacts folder
+and initiate_data_ingestion is start ingestion of the data and convert data into train test split
+"""
+class DataIngestion():
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+         
+    def  initiate_data_ingestion(self):
+        logging.info('Entered the data ingestion method or component')
+        try:
+            df = pd.read_csv('notebook\\data\\stud.csv')
+            logging.info('read data as dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+            logging.info("Train test split initiated")
+            train_set,test_set = train_test_split(df,test_size=0.2,random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info("Ingestion of the data iss completed")
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+if __name__ == "__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
