d230fd24a18e7811764141696b0ef65d6640ed80
vinayak1514 <vinayakjangir1514@gmail.com>
2023-10-30 11:38:37 +0530

    add evaluation fucntion

--- src/utils.py
+++ src/utils.py
@@ -4,9 +4,9 @@ import sys
 import numpy as np 
 import pandas as pd
 import dill
-
+import pickle
 from src.exception import CustomException
-
+from sklearn.metrics import r2_score
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -14,7 +14,20 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
+def evaluate_models(X_train,X_test,y_train,y_test,models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            model.fit(X_train,y_train)
+            y_pred = model.predict(X_test)
+
+            r2_square = r2_score(y_test,y_pred)
+            report[list(models.keys())[i]] = r2_square
+        return report
+    except Exception as e:
+        raise CustomException(e,sys)
