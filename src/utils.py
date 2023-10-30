7e89edccf81cbf52d2a25788e894709e2abf1165
vinayak1514 <vinayakjangir1514@gmail.com>
2023-10-30 12:09:48 +0530

    some change

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import dill
 import pickle
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -18,11 +19,15 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
-def evaluate_models(X_train,X_test,y_train,y_test,models):
+def evaluate_models(X_train,X_test,y_train,y_test,models,param):
     try:
         report = {}
         for i in range(len(models)):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+            gridcv = GridSearchCV(model,para,cv=3)
+            gridcv.fit(X_train,y_train)
+            model.set_params(**gridcv.best_params_)
             model.fit(X_train,y_train)
             y_pred = model.predict(X_test)
 
