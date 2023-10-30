26b9f9de46249178b733f4fbe9b7d023ab92a70e
vinayak1514 <vinayakjangir1514@gmail.com>
2023-10-30 11:37:52 +0530

    add model train pipeline

--- src/pipline/train_pipline.py
+++ src/pipline/train_pipline.py
@@ -1,5 +1,6 @@
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 import os
 import sys 
 import pandas as pd 
@@ -9,4 +10,7 @@ from src.exception import CustomException
 obj =DataIngestion()
 train_data_path,test_data_path = obj.initiate_data_ingestion()
 data_transformation = DataTransformation()
-data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+train_arr , test_arr = data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+model = ModelTrainer()
+model.initiate_model_trainer(train_arr,test_arr)
