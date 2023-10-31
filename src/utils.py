82321b86aeb1e7d91afa0814b54787fbc4ba4dda
vinayak1514 <vinayakjangir1514@gmail.com>
2023-10-31 08:29:59 +0530

    noting change

--- src/utils.py
+++ src/utils.py
@@ -36,3 +36,9 @@ def evaluate_models(X_train,X_test,y_train,y_test,models,param):
         return report
     except Exception as e:
         raise CustomException(e,sys)
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
